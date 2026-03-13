5085300d4f0a679d885daea7bc5fedd2efab4aac
Manikandan R <manikandan.r@intimetec.com>
2026-03-13 14:57:07 +0530

    hello

--- Problems/prime_number.py
+++ Problems/prime_number.py
@@ -2,4 +2,5 @@ start = int(input("Enter start number: "))
 end = int(input("Enter end number: "))
 primes = [num for num in range(start, end + 1)
           if num > 1 and all(num % i != 0 for i in range(2, num))]
+print("prime number series")
 print("Prime Numbers:", primes)
